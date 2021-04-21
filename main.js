9294e776d8db443ad582c899fcad7f72ee4a9e43
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-21 14:14:41 -0400

    pulled button event listeners out into its own function...idk

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/***********
+ * ON LOAD *
+ ***********/
+
+addQuestionButtons();
+addQuestionButtonEventListeners();
+
 /*************
  * SELECTORS *
  *************/
@@ -7,7 +14,6 @@ const questionArea = document.getElementById("question-area");
 /******************
  * EVENT LISTENER *
  ******************/
-addQuestionButtons()
 
 /********************
  * HELPER FUNCTIONS *
@@ -17,62 +23,58 @@ function testSelection() {
 	console.log("You are selecting the element correctly");
 }
 
-function testClick(){
-    console.log("The button was clicked")
+function testClick() {
+	console.log("The button was clicked");
 }
 
-function addQuestionButtons(){
+function addQuestionButtons() {
+	/******************************************
+	 * CREATING A GRID OF ELEMENTS ON THE DOM *
+	 ******************************************/
 
-    /******************************************
-     * CREATING A GRID OF ELEMENTS ON THE DOM *
-     ******************************************/
-    
-    
-    const container = $('#question-area');
-    
-    const NUM_COLS = 5;
-    const DOLLAR_AMOUNTS = ['$100', '$200', '$400', '$600', '$800'];
-    
-    // Loop through each column...
-    for (let i = 0; i < NUM_COLS; i++) {
-    
-        // Create a new column
-        const column = $('<div class="column"></div>');
-    
-        // Loop through each dollar amount..
-        for (const dollarAmount of DOLLAR_AMOUNTS) {
-    
-            // Create a new button
-            const valueButton = $(`<button class="value-select">${dollarAmount}</button>`);
-    
-            // Add item to column
-            column.append(valueButton);
-        }
-    
-        // Add column to container
-        container.append(column);
-    }
-    
-    
-    /****************************************************
-     * ADDING CLICK LISTENERS TO EACH ITEM USING JQUERY *
-     ****************************************************/
-    
-    
-    // Get all buttons
-    const jqueryButtons = $('.value-select');
-    
-    // Loop through the bututons
-    for (const rawButton of jqueryButtons) {
-        
-        // Get the jQuery version of each button
-        const button = $(rawButton);
-    
-        // Add a click listener to each button
-        button.click(function () {
-    
-            // Finally, log the buttons text
-            console.log(button.text());
-        });
-    }
+	const container = $("#question-area");
+
+	const NUM_COLS = 5;
+	const DOLLAR_AMOUNTS = ["$100", "$200", "$400", "$600", "$800"];
+
+	// Loop through each column...
+	for (let i = 0; i < NUM_COLS; i++) {
+		// Create a new column
+		const column = $('<div class="column"></div>');
+
+		// Loop through each dollar amount..
+		for (const dollarAmount of DOLLAR_AMOUNTS) {
+			// Create a new button
+			const valueButton = $(
+				`<button class="value-select">${dollarAmount}</button>`
+			);
+
+			// Add item to column
+			column.append(valueButton);
+		}
+
+		// Add column to container
+		container.append(column);
+	}
+}
+
+function addQuestionButtonEventListeners() {
+	/****************************************************
+	 * ADDING CLICK LISTENERS TO EACH ITEM USING JQUERY *
+	 ****************************************************/
+
+	// Get all buttons
+	const jqueryButtons = $(".value-select");
+
+	// Loop through the bututons
+	for (const rawButton of jqueryButtons) {
+		// Get the jQuery version of each button
+		const button = $(rawButton);
+
+		// Add a click listener to each button
+		button.click(function () {
+			// Finally, log the buttons text
+			console.log(button.text());
+		});
+	}
 }
